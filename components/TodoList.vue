ab2bfc8b8bee8052b4368b7624d3796b333ee40f
Arthur Lee <amurrro@gmail.com>
2021-10-09 08:55:07 +0700

    add deps

---
+++ components/TodoList.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>My Todo App!</h1>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+export default {
+  target: 'static',
+  head: {
+    title: 'idaproject test job',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', 
+      href: '/favicon.ico' }
+    ]
+  },
+  css: ['~/assets/css/index'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    // '~/plugins/components'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
